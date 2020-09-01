98f4e111de730d085e31ea2c70612414caaf81c0
zielmarzouqy <zi.elmarzouqy@gmail.com>
2020-09-02 00:13:27 +0200

    conf 1

--- src/main/java/com/gbiblio/main/A.java
+++ src/main/java/com/gbiblio/main/A.java
@@ -1,9 +1,9 @@
 package com.gbiblio.main;
 
 public class A {
-	//aa a conf git
-	// bbbb conf git
-	// conf github A
+
+	//aa a conf 1
+	// bbbb
 	public void get(float a) {
 		System.out.println(a);
 	}
